3208881ee971541613a243335be2423e8f1ac04e
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2022-05-04 16:18:23 -0300

    chore: install and setup fontawesome

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import { AppProps } from 'next/app';
 import '@/styles/global.css';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fab, faCoffee);
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
