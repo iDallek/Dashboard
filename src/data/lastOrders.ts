adde78ef47ffe2a2116f3c7820c27a2e34bec24c
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2022-05-04 19:48:06 -0300

    feat: create fake data for lastOrders

---
+++ src/data/lastOrders.ts
@@ -0,0 +1,12 @@
+import { faker } from '@faker-js/faker';
+
+export default Array.from({ length: 20 }, () => ({
+  orderId: faker.datatype.number({ min: 10000000, max: 10999999 }),
+  orderDate: faker.datatype.number({ min: 20200000, max: 20221231 }),
+  totalPrice: faker.commerce.price(100, 1000),
+  buyer: faker.name.findName(),
+  status: faker.helpers.arrayElement([
+    [`Pronto para embalar`, `Marcar como embalado`],
+    [`Pronto para envio`, `Notificar envio`],
+  ]),
+}));
